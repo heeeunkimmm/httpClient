682ec595ee3df12e184d23471d4ffb62bb983613
Hannah Kim <h.heeeun.kim@gmail.com>
2017-07-31 18:49:08 +0000

    step 1 finished

---
+++ step1.js
@@ -0,0 +1,23 @@
+var https = require('https');
+
+function getAndPrintHTMLChunks () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  https.get(requestOptions, function (response) {
+    response.setEncoding('utf8');
+
+    response.on('data', function(data) {
+      console.log('Chunk Received: ', data, "\n");
+    });
+
+    response.on('end', function() {
+    console.log("Ended GET request.")
+    });
+  });
+}
+
+console.log(getAndPrintHTMLChunks())
